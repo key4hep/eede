e82ba4c81c4e98f4e4eebc3f82dee4bda74518db
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-06-13 08:46:42 -0500

    implement @tmadlener random color generator

--- js/types/links.js
+++ js/types/links.js
@@ -4,38 +4,8 @@ export const colors = {
   // more if needed
 };
 
-const hexCharacters = [
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-];
-
-function getCharacter(index) {
-  return hexCharacters[index];
-}
-
 export function generateRandomColor() {
-  let hexColorRep = "#";
-
-  for (let index = 0; index < 6; index++) {
-    const randomPosition = Math.floor(Math.random() * hexCharacters.length);
-    hexColorRep += getCharacter(randomPosition);
-  }
-
-  return hexColorRep;
+  return "#" + ((0xffffff * Math.random()) << 0).toString(16).padStart(6, "0");
 }
 
 export class Link {
