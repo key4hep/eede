77cc0ebb2cee08545a945f35b80f68cc3c1e4846
brauliorivas <brauliorivas@users.noreply.github.com>
2024-06-24 20:13:57 +0000

    Deploy preview for PR 42 🛫

--- pr-preview/pr-42/js/information.js
+++ pr-preview/pr-42/js/information.js
@@ -1,5 +1,35 @@
 const infoIcon = document.getElementById("information-icon");
 const closeIcon = document.getElementById("close-information");
+const copyToClipboardButtons =
+  document.getElementsByClassName("copy-email-button");
+
+Array.from(copyToClipboardButtons).forEach((button) => {
+  button.addEventListener("click", () => {
+    const email = button.getAttribute("data-email");
+    copyEmailAddress(email)
+      .then(() => {
+        showCopyInfo(button, "Copied!");
+      })
+      .catch((error) => {
+        showCopyInfo(button, "Error!");
+        console.error("Failed to copy email address: ", error);
+      });
+  });
+});
+
+function showCopyInfo(button, message) {
+  const copyDiv = document.createElement("div");
+  copyDiv.className = "copy-infobox";
+  copyDiv.appendChild(document.createTextNode(message));
+  button.appendChild(copyDiv);
+  setTimeout(() => {
+    button.removeChild(copyDiv);
+  }, 500);
+}
+
+function copyEmailAddress(address) {
+  return navigator.clipboard.writeText(address);
+}
 
 const showModal = () => {
   const modal = document.getElementById("information-modal");
