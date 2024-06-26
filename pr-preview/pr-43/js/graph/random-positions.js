cfb335ac4ad110e1a83dc71d5ea21ea15d5ca8b6
brauliorivas <brauliorivas@users.noreply.github.com>
2024-06-26 03:02:06 +0000

    Deploy preview for PR 43 🛫

--- pr-preview/pr-43/js/graph/random-positions.js
+++ pr-preview/pr-43/js/graph/random-positions.js
@@ -1,5 +1,8 @@
 import { canvas } from "../main.js";
 
+const boxWidth = 120;
+const boxHeight = 240;
+
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -10,8 +13,8 @@ export function generateRandomPositions(nodes) {
 
   for (const node of nodes) {
     if (isNaN(node.x) && isNaN(node.y)) {
-      node.x = randomNumber(0, width);
-      node.y = randomNumber(0, height);
+      node.x = randomNumber(0, width - boxWidth);
+      node.y = randomNumber(0, height - boxHeight);
     }
   }
 }
