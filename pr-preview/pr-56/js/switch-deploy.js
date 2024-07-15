7521f0402e2e82a74bfc5f5cdb16158d03d68cde
brauliorivas <brauliorivas@users.noreply.github.com>
2024-07-15 22:16:22 +0000

    Deploy preview for PR 56 🛫

--- pr-preview/pr-56/js/switch-deploy.js
+++ pr-preview/pr-56/js/switch-deploy.js
@@ -3,9 +3,7 @@ const button = document.getElementById("switch-deploy-button");
 button.addEventListener("click", () => {
   const currentUrl = window.location.href;
 
-  if (currentUrl.includes("/main")) {
-    window.location.href = currentUrl.replace("/main", "/release");
-  } else if (currentUrl.includes("/release")) {
+  if (currentUrl.includes("/release")) {
     window.location.href = currentUrl.replace("/release", "/main");
   } else {
     window.location.href = "https://key4hep.github.io/eede/release/index.html";
@@ -13,10 +11,8 @@ button.addEventListener("click", () => {
 });
 
 const url = window.location.href;
-if (url.includes("/main")) {
-  button.innerText = "Release";
-} else if (url.includes("/release")) {
-  button.innerText = "Main";
+if (url.includes("/release")) {
+  button.innerText = "Develop";
 } else {
   button.innerText = "Release";
 }
