28b2d709bcd518aec0a60a702e29e31df1d54ba4
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-07-15 17:45:39 -0500

    parse bitfield according to @tmadlener bitwise operations

--- mappings/sim-status.js
+++ mappings/sim-status.js
@@ -8,3 +8,27 @@ export const SimStatusBitFieldDisplayValues = {
   29: "Backscatter",
   30: "CreatedInSimulation",
 };
+
+export function parseBits(bit) {
+  const bits = [];
+
+  for (let i = 0; i < 32; i++) {
+    if (bit & (1 << i)) {
+      bits.push(i);
+    }
+  }
+
+  return bits;
+}
+
+export function getSimStatusDisplayValues(bits) {
+  return bits.map((bit) =>
+    SimStatusBitFieldDisplayValues[bit] !== undefined
+      ? SimStatusBitFieldDisplayValues[bit]
+      : `Bit ${bit}`
+  );
+}
+
+export function getSimStatusDisplayValuesFromBit(bit) {
+  return getSimStatusDisplayValues(parseBits(bit));
+}
