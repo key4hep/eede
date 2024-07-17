0decc8ce2ec23fdba9c4275ce3a52feac42aa758
brauliorivas <brauliorivas@users.noreply.github.com>
2024-07-17 15:12:18 +0000

    Remove preview for PR 58 🛬

--- pr-preview/pr-58/js/filter/nofilter.js
+++
@@ -1,7 +0,0 @@
-export function setupNoFilter() {
-  const manipulationTools =
-    document.getElementsByClassName("manipulation-tool");
-  for (const tool of manipulationTools) {
-    tool.style.display = "none";
-  }
-}
--- pr-preview/pr-58/js/views/scrolls.js
+++
@@ -1,9 +0,0 @@
-import { canvas } from "../main.js";
-
-export function scrollTopCenter() {
-  return { x: (canvas.width - window.innerWidth) / 2, y: 0 };
-}
-
-export function scrollTopLeft() {
-  return { x: 0, y: 0 };
-}
