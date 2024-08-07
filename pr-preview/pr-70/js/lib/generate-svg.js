7b265725c42b1e7df7079bc7638add879562bd5e
brauliorivas <brauliorivas@users.noreply.github.com>
2024-08-07 23:19:56 +0000

    Deploy preview for PR 70 🛫

--- pr-preview/pr-70/js/lib/generate-svg.js
+++ pr-preview/pr-70/js/lib/generate-svg.js
@@ -1,17 +1,28 @@
-const SCALE = 1.25;
-
 const store = {};
 
-export async function textToSVG(id, text, size) {
+export async function textToSVG(id, text, maxWidth, maxHeight) {
   if (store[id]) {
     return store[id];
   }
 
-  const mathjaxContainer = await MathJax.tex2svgPromise(`${text}`);
+  const mathjaxContainer = await MathJax.tex2svgPromise(`${text}`, {});
   const svg = mathjaxContainer.firstElementChild;
 
-  svg.setAttribute("width", `${parseInt(size * SCALE)}px`);
-  svg.setAttribute("height", `${parseInt(size * SCALE)}px`);
+  const width = parseFloat(svg.getAttribute("width").replace("ex", ""));
+  const height = parseFloat(svg.getAttribute("height").replace("ex", ""));
+
+  const imageRatio = width / height;
+
+  let finalHeight = maxHeight;
+  let finalWidth = parseInt(finalHeight * imageRatio);
+
+  if (finalWidth > maxWidth) {
+    finalWidth = maxWidth;
+    finalHeight = parseInt(finalWidth / imageRatio);
+  }
+
+  svg.setAttribute("width", `${finalWidth}px`);
+  svg.setAttribute("height", `${finalHeight}px`);
 
   const src =
     "data:image/svg+xml;base64," +
