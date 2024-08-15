6a7bcdbea13333b8817c037ae4d77d06053f649f
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-08-15 10:53:07 -0500

    fix tree views, and don't generate new connections TODO: implement in mcparticle tree

--- js/filters/reconnect/mcparticletree.js
+++ js/filters/reconnect/mcparticletree.js
@@ -16,38 +16,39 @@ const findDaughterRow = (object, uniqueRows, rowToIndex) => {
   return NaN;
 };
 
-export function reconnectMCParticleTree(viewCurrentObjects) {
+export function reconnectMCParticleTree(viewCurrentObjects, ids) {
   const { collection, oneToMany } =
     viewCurrentObjects.datatypes["edm4hep::MCParticle"];
 
   const sortedCollection = collection.sort((a, b) => a.row - b.row);
 
-  const beginRowsIndex = {};
-  sortedCollection.forEach((object, index) => {
-    if (beginRowsIndex[object.row] === undefined) {
-      beginRowsIndex[object.row] = index;
-    }
-  });
+  // const beginRowsIndex = {};
+  // sortedCollection.forEach((object, index) => {
+  //   if (beginRowsIndex[object.row] === undefined) {
+  //     beginRowsIndex[object.row] = index;
+  //   }
+  // });
 
-  const rows = sortedCollection.map((object) => object.row);
-  const uniqueRows = [...new Set(rows)];
+  // const rows = sortedCollection.map((object) => object.row);
+  // const uniqueRows = [...new Set(rows)];
 
-  const rowToIndex = {};
-  for (const [index, row] of uniqueRows.entries()) {
-    rowToIndex[row] = index;
-  }
+  // const rowToIndex = {};
+  // for (const [index, row] of uniqueRows.entries()) {
+  //   rowToIndex[row] = index;
+  // }
 
-  const rowToObjectsCount = {};
+  // const rowToObjectsCount = {};
 
-  sortedCollection.forEach((object) => {
-    if (rowToObjectsCount[object.row] === undefined) {
-      rowToObjectsCount[object.row] = 1;
-      return;
-    }
-    rowToObjectsCount[object.row] += 1;
-  });
+  // sortedCollection.forEach((object) => {
+  //   if (rowToObjectsCount[object.row] === undefined) {
+  //     rowToObjectsCount[object.row] = 1;
+  //     return;
+  //   }
+  //   rowToObjectsCount[object.row] += 1;
+  // });
 
   for (const object of sortedCollection) {
+    const { oneToManyRelations } = object;
     object.saveRelations();
 
     object.oneToManyRelations = {
@@ -55,34 +56,34 @@ export function reconnectMCParticleTree(viewCurrentObjects) {
       "daughters": [],
     };
 
-    const parentRow = findParentRow(object, uniqueRows, rowToIndex);
-    if (parentRow !== NaN) {
-      const beginIndex = beginRowsIndex[parentRow];
-      const endIndex = beginIndex + rowToObjectsCount[parentRow];
+    // const parentRow = findParentRow(object, uniqueRows, rowToIndex);
+    // if (parentRow !== NaN) {
+    //   const beginIndex = beginRowsIndex[parentRow];
+    //   const endIndex = beginIndex + rowToObjectsCount[parentRow];
 
-      for (let i = beginIndex; i < endIndex; i++) {
-        const newParentLink = new linkTypes["parents"](
-          object,
-          sortedCollection[i]
-        );
-        object.oneToManyRelations["parents"].push(newParentLink);
-        oneToMany["parents"].push(newParentLink);
-      }
-    }
+    //   for (let i = beginIndex; i < endIndex; i++) {
+    //     const newParentLink = new linkTypes["parents"](
+    //       object,
+    //       sortedCollection[i]
+    //     );
+    //     object.oneToManyRelations["parents"].push(newParentLink);
+    //     oneToMany["parents"].push(newParentLink);
+    //   }
+    // }
 
-    const daughterRow = findDaughterRow(object, uniqueRows, rowToIndex);
-    if (daughterRow !== NaN) {
-      const beginIndex = beginRowsIndex[daughterRow];
-      const endIndex = beginIndex + rowToObjectsCount[daughterRow];
+    // const daughterRow = findDaughterRow(object, uniqueRows, rowToIndex);
+    // if (daughterRow !== NaN) {
+    //   const beginIndex = beginRowsIndex[daughterRow];
+    //   const endIndex = beginIndex + rowToObjectsCount[daughterRow];
 
-      for (let i = beginIndex; i < endIndex; i++) {
-        const newDaughterLink = new linkTypes["daughters"](
-          object,
-          sortedCollection[i]
-        );
-        object.oneToManyRelations["daughters"].push(newDaughterLink);
-        oneToMany["daughters"].push(newDaughterLink);
-      }
-    }
+    //   for (let i = beginIndex; i < endIndex; i++) {
+    //     const newDaughterLink = new linkTypes["daughters"](
+    //       object,
+    //       sortedCollection[i]
+    //     );
+    //     object.oneToManyRelations["daughters"].push(newDaughterLink);
+    //     oneToMany["daughters"].push(newDaughterLink);
+    //   }
+    // }
   }
 }
