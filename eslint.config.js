ad156642e38ebfc337da7934b1ac8d9056640aa3
Juraj Smiesko <34742917+kjvbrt@users.noreply.github.com>
2025-08-19 13:28:05 +0200

    Splitting old input modal into two: welcome modal and input modal (#96)
    * Ignoring subset collections and storing event numbers in the session storage
    
    * Started concentrating global state into one file, added ESLint
    
    * Moved serve.js to bin/ folder
    
    * Split Input modal into file input and welcome modal
    
    * Adding warning message and EDM4hep link
    
    * Fix issues rised by the tests
    
    * Correcting edm4hep2json flags

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
