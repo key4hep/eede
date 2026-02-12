c6e6eb1202762a0bef8608c76c58b902689ba97b
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2026-02-12 16:33:09 +0100

    Pass tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 export default {
   transform: {},
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "pixi-viewport": "<rootDir>/test/__mocks__/pixi-viewport.js",
+    "pixi\\.min\\.mjs$": "<rootDir>/test/__mocks__/pixi.js",
+  },
 };
