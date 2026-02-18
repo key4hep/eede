c69eac069f222ed5655305d24cdfedd594c40e15
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2026-02-18 15:51:11 +0100

    Update imports

--- js/loaders/loadObjects.js
+++ js/loaders/loadObjects.js
@@ -1,4 +1,4 @@
-import { objectTypes } from "../viz/objects/objects.js";
+import { objectTypes } from "../viz/constants/objects.js";
 import { supportedEDM4hepTypes } from "../../model/datatypes.js";
 
 export function getSupportedEDM4hepTypes(schemaVersion) {
