9a4fd3c58cb8b47fd232521c2bcc3764b2991fda
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2026-02-23 10:28:48 +0100

    Restore globals file [Read more]
    Remove individual files

--- js/state/schema.js
+++
@@ -1,7 +0,0 @@
-export function setCurrentSchemaVersion(schemaVersion) {
-  window.sessionStorage.setItem("current-schema-version", schemaVersion);
-}
-
-export function getCurrentSchemaVersion() {
-  return window.sessionStorage.getItem("current-schema-version");
-}
