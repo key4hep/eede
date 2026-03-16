cf00fb1892860eccbf9dcb3361ecdb41a2487fe1
pablo <74920898+apausa@users.noreply.github.com>
2026-03-16 14:57:59 +0100

    Clean code

--- js/filters/reconnect.js
+++ js/filters/reconnect.js
@@ -1,43 +1,17 @@
-export function reconnect(
-  viewCurrentObjects,
-  collectionsNames,
-  ids,
-  reconnectFunction
-) {
+export function reconnect(viewCurrentObjects, collectionsNames) {
   for (const collectionName of collectionsNames) {
-    const { collection, oneToOne, oneToMany } =
-      viewCurrentObjects.datatypes[collectionName];
+    const { collection } = viewCurrentObjects.datatypes[collectionName];
 
     for (const object of collection) {
       const { oneToManyRelations, oneToOneRelations } = object;
       object.saveRelations();
 
-      for (const [relationName, relations] of Object.entries(
-        oneToManyRelations
-      )) {
+      for (const [relationName] of Object.entries(oneToManyRelations)) {
         object.oneToManyRelations[relationName] = [];
-
-        for (const relation of relations) {
-          const toObject = relation.to;
-          const toObjectId = `${toObject.index}-${toObject.collectionId}`;
-
-          if (ids.has(toObjectId)) {
-          } else {
-          }
-        }
       }
 
-      for (const [relationName, relation] of Object.entries(
-        oneToOneRelations
-      )) {
+      for (const [relationName] of Object.entries(oneToOneRelations)) {
         object.oneToOneRelations[relationName] = null;
-
-        const toObject = relation.to;
-        const toObjectId = `${toObject.index}-${toObject.collectionId}`;
-
-        if (ids.has(toObjectId)) {
-        } else {
-        }
       }
     }
   }
