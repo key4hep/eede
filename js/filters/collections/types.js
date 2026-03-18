1a30ab20f3ab5ffead3e68f2949d407c772d659a
pablo <74920898+apausa@users.noreply.github.com>
2026-03-18 10:28:58 +0100

    Remove individual filter collections

--- js/filters/collections/types.js
+++
@@ -1,6 +0,0 @@
-export const MCPARTICLE = "edm4hep::MCParticle";
-export const RECOPARTICLE = "edm4hep::ReconstructedParticle";
-export const CLUSTER = "edm4hep::Cluster";
-export const TRACK = "edm4hep::Track";
-export const VERTEX = "edm4hep::Vertex";
-export const PARTICLEID = "edm4hep::ParticleID";
