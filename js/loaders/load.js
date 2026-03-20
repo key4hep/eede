736103c5fbe3df904a6731434a63591c5bd4ce88
pablo <74920898+apausa@users.noreply.github.com>
2026-03-20 11:42:25 +0100

    Handle schema 6 as schema 3

--- js/loaders/load.js
+++ js/loaders/load.js
@@ -3,7 +3,6 @@ import { handleOldEvent } from "./loadOldSchemaEvent.js";
 import { handleSchema1Event } from "./loadSchema1Event.js";
 import { handleSchema2Event } from "./loadSchema2Event.js";
 import { handleSchema3Event } from "./loadSchema3Event.js";
-import { handleSchema4Event } from "./loadSchema4Event.js";
 
 export function loadObjects(fileData, eventNum) {
   const eventData = fileData["Event " + eventNum];
@@ -13,22 +12,29 @@ export function loadObjects(fileData, eventNum) {
   }
 
   const schemaVersion = determineSchemaVersion(eventData);
+
   if (typeof schemaVersion === "undefined") {
     return;
   }
+
   setCurrentSchemaVersion(schemaVersion);
+
   console.log(`Info: schemaVersion = ${schemaVersion}`);
 
-  if (schemaVersion === "old") {
-    return handleOldEvent(eventData);
-  } else if (schemaVersion === 1) {
-    return handleSchema1Event(eventData);
-  } else if (schemaVersion === 2) {
-    return handleSchema2Event(eventData);
-  } else if (schemaVersion === 3) {
-    return handleSchema3Event(eventData);
-  } else if (schemaVersion === 4) {
-    return handleSchema4Event(eventData);
+  switch (schemaVersion) {
+    case "old":
+      handleOldEvent(eventData);
+      break;
+    case 1:
+      handleSchema1Event(eventData);
+      break;
+    case 2:
+      handleSchema2Event(eventData);
+      break;
+    case 3:
+    case 6:
+      handleSchema3Event(eventData);
+      break;
   }
 }
 
@@ -37,40 +43,41 @@ function determineSchemaVersion(eventData) {
   const schemaVersions = Object.values(eventData).map(
     (c) => c.collSchemaVersion,
   );
+
   if (!schemaVersions.every((v) => v === undefined)) {
     return Math.max(...schemaVersions.filter((v) => v !== undefined));
   }
 
   // Find schema version from the EDM4hep version
   const edm4hepVersion = String(eventData.edm4hepVersion);
-  if (typeof edm4hepVersion !== "undefined") {
-    console.log(`Info: EDM4hep version = ${edm4hepVersion}`);
-    const schema1versions = [
-      "0.9.0",
-      "0.10.0",
-      "0.10.1",
-      "0.10.2",
-      "0.10.3",
-      "0.10.4",
-      "0.10.5",
-      "0.10.99",
-    ];
-    const schema2versions = ["0.99.0", "0.99.1"];
-    const schema3versions = ["0.99.2"];
-    if (schema1versions.includes(edm4hepVersion)) {
-      return 1;
-    } else if (schema2versions.includes(edm4hepVersion)) {
-      return 2;
-    } else if (schema3versions.includes(edm4hepVersion)) {
-      return 3;
-    }
 
-    if (Number(edm4hepVersion.split(".")[1]) < 9) {
-      return "old";
+  console.log(`Info: EDM4hep version = ${edm4hepVersion}`);
+
+  if (!edm4hepVersion) return undefined;
+
+  switch (edm4hepVersion) {
+    case "0.9.0":
+    case "0.10.0":
+    case "0.10.1":
+    case "0.10.2":
+    case "0.10.3":
+    case "0.10.4":
+    case "0.10.5":
+    case "0.10.99":
+      return 1;
+    case "0.99.0":
+    case "0.99.1":
+      return 2;
+    case "0.99.2":
+      return 3;
+    case "1.0.0":
+      return 6;
+    default: {
+      const version = edm4hepVersion.split(".");
+
+      return Number(version[0]) === 0 && Number(version[1]) < 9
+        ? "old"
+        : undefined;
     }
   }
-
-  // TODO: Should return undefined and an erorr that the file in no longer supported.
-  console.log("Very old file!");
-  return "old";
 }
