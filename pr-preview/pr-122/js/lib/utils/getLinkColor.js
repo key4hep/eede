4308e71456b1783306f4e54d000902201677e5fe
apausa <apausa@users.noreply.github.com>
2026-07-24 14:55:43 +0000

    Remove preview for PR 122 🛬

--- pr-preview/pr-122/js/lib/utils/getLinkColor.js
+++
@@ -1,6 +0,0 @@
-import { filteredOut } from "../constants/vizStyles.js";
-
-export function getLinkColor(link) {
-  const filtered = link.from.filteredOut || link.to.filteredOut;
-  return filtered ? filteredOut.linkColor : link.color;
-}
--- pr-preview/pr-122/js/viz/views/buildStructures/buildList.js
+++
@@ -1,8 +0,0 @@
-import { emptyCopyObject } from "../../../lib/utils/copy.js";
-
-export function buildList(currentObjects, viewObjects, collectionName) {
-  emptyCopyObject(currentObjects, viewObjects);
-
-  viewObjects.datatypes[collectionName].collection =
-    currentObjects.datatypes[collectionName].collection;
-}
